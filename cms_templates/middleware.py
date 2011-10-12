24b3bad4b09460467b4f4576bdbfdf6820ce085e
Sever Băneşiu <banesiu.sever@gmail.com>
2011-10-12 11:20:56 +0300

    Changed site ID patching algorithm.

--- cms_templates/middleware.py
+++ cms_templates/middleware.py
@@ -1,11 +1,30 @@
 from django.conf import settings
+from django.core.urlresolvers import resolve
 from djangotoolbox.utils import make_tls_property
+from djangotoolbox.sites.dynamicsite import DynamicSiteIDMiddleware
 from django.contrib.sites.models import Site
 from dbtemplates.models import Template
 from cms.models import Page
 
 
 CMS_TEMPLATES = settings.__class__.CMS_TEMPLATES = make_tls_property()
+
+class SiteIDPatchMiddleware(object):
+    """ This middleware works together with DynamicSiteIDMiddleware
+    from djangotoolbox and patches the site_id based on the
+    django-cms cms_admin_site session variable if in admin or
+    based on the domain by falling back on DynamicSiteIDMiddleware.
+    """
+    fallback = DynamicSiteIDMiddleware()
+
+    def process_request(self, request):
+        site_id = request.session.get('cms_admin_site', None)
+        match = resolve(request.path)
+        if match.app_name in ['admin'] and site_id:
+            settings.__class__.SITE_ID.value = site_id
+        else:
+            self.fallback.process_request(request)
+
 
 class DBTemplatesMiddleware(object):
     def process_request(self, request):
