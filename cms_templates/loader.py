439bf6d3ffeb49628667e79d8c54e831c40cacea
Marcel Cojocaru <marcel.cojocaru@threepillarglobal.com>
2013-01-31 16:49:38 +0200

    LUN-368 -- small code refactor

--- cms_templates/loader.py
+++ cms_templates/loader.py
@@ -5,9 +5,8 @@ from django.contrib.sites.models import Site
 class CmsTemplatesLoader(Loader):
 
     def load_and_store_template(self, template_name, cache_key, site, **params):
-        sites_in = params.get('sites__in', None)
-        if sites_in:
-            site_ids = [s.id for s in Site.objects.filter(name__in=shared_sites)] + sites_in
+        if params.has_key('sites__in'):
+            site_ids =  params['sites__in'] + [s.id for s in Site.objects.filter(name__in=shared_sites)]
             return super(CmsTemplatesLoader, self).load_and_store_template(
                 template_name, cache_key, site, sites__in=site_ids)
         return super(CmsTemplatesLoader, self).load_and_store_template(
