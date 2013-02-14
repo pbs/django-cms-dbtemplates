1c5523943e104e18d7b158044707607384d51221
Bogdan Hodorog <bogdan.hodorog@threepillarglobal.com>
2013-02-14 12:31:46 +0200

    support files for nose runner

---
+++ runtests.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from os.path import dirname, abspath
+from optparse import OptionParser
+
+sys.path.insert(0, dirname(abspath(__file__)))
+
+
+from django.conf import settings
+if not settings.configured:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_test'
+
+from django_nose import NoseTestSuiteRunner
+
+def runtests(*test_args, **kwargs):
+    if 'south' in settings.INSTALLED_APPS:
+        from south.management.commands import patch_for_test_db_setup
+        patch_for_test_db_setup()
+
+    kwargs.setdefault('interactive', False)
+
+    test_runner = NoseTestSuiteRunner(**kwargs)
+
+    failures = test_runner.run_tests(test_args)
+    sys.exit(failures)
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option('--verbosity', dest='verbosity', action='store', default=1, type=int)
+    parser.add_options(NoseTestSuiteRunner.options)
+    (options, args) = parser.parse_args()
+
+    runtests(*args, **options.__dict__)
