67a26441792852543713f395ab940b9cfc32941a
Bogdan Hodorog <bogdan.hodorog@3pillarglobal.com>
2013-02-25 10:26:27 +0200

    Remove pbs_test_runner and replace it with a smaller runtests.py settings.py for tests are now in a separate file.

--- runtests.py
+++ runtests.py
@@ -13,26 +13,26 @@ if not settings.configured:
 from django_nose import NoseTestSuiteRunner
 
 
-def runtests(*test_args, **kwargs):
-    if 'south' in settings.INSTALLED_APPS:
-        from south.management.commands import patch_for_test_db_setup
-        patch_for_test_db_setup()
+class TestsWrapper(unittest.TestCase):
+    def __init__(self, *test_args, **kwargs):
+        super(TestsWrapper, self).__init__()
+        from django.conf import settings
+        if not settings.configured:
+            os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_test'
+        if 'south' in settings.INSTALLED_APPS:
+            from south.management.commands import patch_for_test_db_setup
+            patch_for_test_db_setup()
+        if not test_args:
+            test_args = ['cms_templates.tests']
+            kwargs.setdefault('interactive', False)
+            test_runner = NoseTestSuiteRunner(**kwargs)
+            self._failures = test_runner.run_tests(test_args)
 
-    if not test_args:
-        test_args = ['cms_templates.tests']
+    def runTest(self):
+        self.assertEqual(self._failures, 0)
 
-    kwargs.setdefault('interactive', False)
-
-    test_runner = NoseTestSuiteRunner(**kwargs)
-    failures = test_runner.run_tests(test_args)
-
-    class TestWrapper(unittest.TestCase):
-        def setUp(self):
-            pass
-        def runTest(self):
-            self.assertEqual(failures, 0)
-
-    return TestWrapper()
+def runtests(*args, **kwargs):
+    return TestsWrapper(*args, **kwargs)
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -40,4 +40,4 @@ if __name__ == '__main__':
                       default=1, type=int)
     parser.add_options(NoseTestSuiteRunner.options)
     (options, args) = parser.parse_args()
-    runtests(*args, **options.__dict__)
+    TestsWrapper(*args, **options.__dict__)
