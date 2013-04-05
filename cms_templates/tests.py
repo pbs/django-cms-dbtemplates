b28b50f562dbd44b4c32c8fdbb6cdebb2945759e
Laura Feier <laura.feier@threepillarglobal.com>
2013-04-05 16:47:43 +0300

    added tests for template validation

--- cms_templates/tests.py
+++ cms_templates/tests.py
@@ -13,6 +13,9 @@ from mock import patch
 from restricted_admin_decorators import (restricted_has_delete_permission,
     restricted_get_readonly_fields, restricted_formfield_for_manytomany,
     restricted_queryset)
+import re
+from cms.models.pagemodel import Page
+
 
 counter = 0
 def create_site(**kwargs):
@@ -435,3 +438,354 @@ class TestLoader(TestCase):
         t_orphan = Template.objects.create(name='orphan.html', content='orphan')
 
         self.assertEqual(loader.get_template('orphan.html').render(Context({})), 'orphan')
+
+
+class TestTemplateValidation(TestCase):
+
+    def setUp(self):
+        username = 'test_templates_user'
+        try:
+            User.objects.get(username__exact=username)
+        except User.DoesNotExist:
+            User.objects.create_superuser(
+                username=username, password='x',
+                email='%s@templates.com' % username)
+        self.client.login(username=username, password='x')
+        session = self.client.session
+        session['cms_admin_site'] = 1
+        session.save()
+
+    def _build_url(self, model, instance_id=None):
+        args, url = ((instance_id, ), "admin:%s_%s_change") if instance_id \
+            else ((), "admin:%s_%s_add")
+        return urlresolvers.reverse(url % (
+            model._meta.app_label, model._meta.module_name),
+        args=args)
+
+    def _site_url(self, site_id=None):
+        return self._build_url(Site, site_id)
+
+    def _templ_url(self, template_id=None):
+        return self._build_url(Template, template_id)
+
+    def _matches_error_message(self, msg, error_pattern):
+        str_pieces = set(re.split('({\d+})', error_pattern)) -\
+            set(re.findall('({\d+})', error_pattern))
+        for str_piece in str_pieces:
+            self.assertTrue(str_piece in msg)
+
+    def assertFormValidationMessage(self, error_msg_key, response):
+        form = response.context['adminform'].form
+        error = form.errors['__all__'][0]
+        self._matches_error_message(
+            error, form.custom_error_messages[error_msg_key])
+
+    def assertTemplatesFormValidationMessage(self, error_msg_key, response):
+        form = response.context['adminform'].form
+        error = form.errors['templates'][0]
+        self._matches_error_message(
+            error, form.custom_error_messages[error_msg_key])
+
+    def _trigger_validation_error_on_template_form(
+            self, name, content, sites, err_key, _id=None):
+        url = self._templ_url(_id) if _id else self._templ_url()
+        response = self.client.post(url,
+            {'name': name, 'content': content, 'sites': sites})
+        self.assertEquals(response.status_code, 200)
+        self.assertFormValidationMessage(err_key, response)
+
+    def _trigger_validation_error_on_site_form(
+            self, name, domain, templates, err_key, _id=None):
+        url = self._site_url(_id) if _id else self._site_url()
+        response = self.client.post(url,
+            {'name': name, 'domain': domain, 'templates': templates})
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplatesFormValidationMessage(err_key, response)
+
+    def _update_template(self, name, content, sites, _id=None):
+        url = self._templ_url(_id) if _id else self._templ_url()
+        response = self.client.post(url,
+            {'name': name, 'content': content, 'sites': sites})
+        self.assertEquals(response.status_code, 302)
+
+    def test_syntax_error(self):
+        self._trigger_validation_error_on_template_form(
+            'templA', '{% dummy %}', [1], 'syntax_error')
+        self._update_template('templA', 'random text', [1])
+
+        templA = Template.objects.get(name='templA')
+        templA.content = '{% dummy %}'
+        templA.save()
+
+        # syntax error in included template
+        self._trigger_validation_error_on_template_form(
+            'templB', '{% extends "templA" %}', [1], 'syntax_error')
+        # syntax error in extended template
+        self._trigger_validation_error_on_template_form(
+            'templB', '{% include "templA" %}', [1], 'syntax_error')
+
+        templC = Template.objects.create(name='templC', content=(''
+                '{% load sekizai_tags %}'
+                '{% render_block "stuff" %}'
+                ))
+        templC.sites.add(Site.objects.get(id=1))
+        templC.save()
+        # syntax error in render block
+        self._trigger_validation_error_on_template_form(
+            'templB',
+            ('{% extends "templC" %}'
+            '{% load sekizai_tags %}'
+            '{% addtoblock "stuff" %}'
+            '{% include "templA" %}'
+            '{% endaddtoblock %}'), [1], 'syntax_error')
+
+        # syntax error in regular block
+        templC.content = (''
+            '{% block content %}'
+            'some content'
+            '{% endblock content %}')
+        templC.save()
+
+        self._trigger_validation_error_on_template_form(
+            'templB',
+            ('{% extends "templC" %}'
+            '{% block content %}'
+            '{% include "templA" %}'
+            '{% endblock content %}'), [1], 'syntax_error')
+
+        # syntax error in super block
+        templC.content = (''
+            '{% block content %}'
+            '{% include "templA" %}'
+            '{% endblock content %}')
+        templC.save()
+
+        self._trigger_validation_error_on_template_form(
+            'templB',
+            ('{% extends "templC" %}'
+            '{% block content %}'
+            '{{ block.super }}'
+            '{% endblock content %}'), [1], 'syntax_error')
+
+        # syntax error even when override
+        self._trigger_validation_error_on_template_form(
+            'templB',
+            ('{% extends "templC" %}'
+            '{% block content %}'
+            'some content'
+            '{% endblock content %}'), [1], 'syntax_error')
+
+    def test_name_field_read_only(self):
+        response = self.client.get(self._templ_url())
+        name_widget = response.context['adminform'].form.fields['name'].widget
+        self.assertNotIn('readonly', name_widget.attrs)
+        self.assertNotIn('disabled', name_widget.attrs)
+        templA = Template.objects.create(
+            name='templA', content="asdasd")
+        templA.sites.add(Site.objects.get(id=1))
+        templA.save()
+
+        response = self.client.get(self._templ_url(templA.id))
+        name_widget = response.context['adminform'].form.fields['name'].widget
+        self.assertIn('readonly', name_widget.attrs)
+        self.assertIn('disabled', name_widget.attrs)
+
+    def test_nonexistent_template_use(self):
+        self._trigger_validation_error_on_template_form(
+            'templB', '{% include "templA" %}', [1], 'missing_template_use')
+        self._trigger_validation_error_on_template_form(
+            'templB', '{% extends "templA" %}', [1], 'missing_template_use')
+
+        templA = Template.objects.create(name='templA', content=(''
+                '{% include "templC" %}'))
+        templA.sites.add(Site.objects.get(id=1))
+        templA.save()
+
+        self._trigger_validation_error_on_template_form(
+            'templB', '{% include "templA" %}', [1], 'missing_template_use')
+        self._trigger_validation_error_on_template_form(
+            'templB', '{% extends "templA" %}', [1], 'missing_template_use')
+
+        templA.content = (
+            '{% block content %}'
+            '{% include "templC" %}'
+            '{% endblock content %}')
+        templA.save()
+
+        self._trigger_validation_error_on_template_form(
+            'templB', '{% include "templA" %}', [1], 'missing_template_use')
+        self._trigger_validation_error_on_template_form(
+            'templB', (
+                '{% extends "templA" %}'
+                '{% block content %}'
+                'texttexttext'
+                '{% endblock content %}'), [1], 'missing_template_use')
+
+        templA.content = (
+            '{% load sekizai_tags %}'
+            '{% render_block "stuff" %}')
+        templA.save()
+
+        self._trigger_validation_error_on_template_form(
+            'templB', (
+                '{% extends "templA" %}'
+                '{% load sekizai_tags %}'
+                '{% addtoblock "stuff" %}'
+                '{% include "templC" %}'
+                '{% endaddtoblock %}'), [1], 'missing_template_use')
+
+    def test_templates_use_sites_assigned(self):
+        templA = Template.objects.create(name='templA')
+        templA.content = 'content'
+        templA.save()
+        templA.sites.clear()
+
+        templB = Template.objects.create(name='templB')
+        templB.content = '{% include "templA" %}'
+        templB.save()
+        templB.sites.clear()
+
+        self._trigger_validation_error_on_template_form(
+            'templC', '{% extends "templB" %}', [1], 'missing_sites')
+
+    def test_sites_unassigned(self):
+        siteA = Site.objects.create(name="siteA", domain="siteA")
+
+        templA = Template.objects.create(name='templA')
+        templA.content = 'content'
+        templA.save()
+        templA.sites.add(siteA)
+
+        p = Page(template="templA", site=siteA)
+        p.save()
+
+        self._trigger_validation_error_on_template_form(
+            'templA', 'content', [1], 'page_use', templA.id)
+        self._update_template(
+            'templA', 'content', [1, siteA.id], templA.id)
+
+        Page.objects.filter(id=p.id).update(template='templNonExistent')
+
+        self._trigger_validation_error_on_template_form(
+            'templA', 'content', [1], 'nonexistent_in_pages', templA.id)
+
+        templB = Template.objects.create(name='templB')
+        templB.content = '{% invalid_tag %}'
+        templB.save()
+        templB.sites.add(siteA)
+
+        Page.objects.filter(id=p.id).update(template='templB')
+
+        self._trigger_validation_error_on_template_form(
+            'templA', 'content', [1], 'syntax_error_unassigning', templA.id)
+
+        templC = Template.objects.create(name='templC')
+        templC.content = '{% include "NonExistent" %}'
+        templC.save()
+        templC.sites.add(siteA)
+
+        templB.content = '{% extends "templC" %}'
+        templB.save()
+
+        Page.objects.filter(id=p.id).update(template='templB')
+
+        self._trigger_validation_error_on_template_form(
+            'templA', 'content', [1], 'missing_template_use', templA.id)
+
+        templC.content = '{% include "templA" %}'
+        templC.save()
+
+        self._trigger_validation_error_on_template_form(
+            'templA', 'content', [1], 'page_template_use', templA.id)
+
+        templD = Template.objects.create(name='templD')
+        templD.content = 'content'
+        templD.save()
+        templD.sites.add(siteA)
+
+        Page.objects.filter(id=p.id).update(template='templD')
+
+        self._trigger_validation_error_on_template_form(
+            'templA', 'content', [1], 'site_template_use', templA.id)
+
+        templB.content = '{% extends "templC" %}{% invalid_tag %}'
+        templB.save()
+
+        self._trigger_validation_error_on_template_form(
+            'templA', 'content', [1], 'syntax_error_unassigning', templA.id)
+
+        templB.content = '{% extends "templC" %}{% include "NonExistent" %}'
+        templB.save()
+
+        self._trigger_validation_error_on_template_form(
+            'templA', 'content', [1], 'missing_template_use', templA.id)
+
+        p.delete()
+        self._trigger_validation_error_on_template_form(
+            'templA', 'content', [1], 'missing_template_use', templA.id)
+
+    def test_site_has_all_templates_required(self):
+        created = []
+        for t_name in ['templA', 'templB', 'templC', 'templD']:
+            templ = Template.objects.create(name=t_name)
+            templ.content = "content"
+            templ.save()
+            created.append(templ)
+        templA, templB, templC, templD = tuple(created)
+
+        templA.content = '{% extends "templB" %}'
+        templB.content = '{% include "templC" %}'
+        templC.content = 'content'
+
+        for t_instance in [templA, templB, templC]:
+            t_instance.save()
+
+        self._trigger_validation_error_on_site_form(
+            "siteA", "siteA.com", [templA.id], 'all_required')
+
+        s = Site.objects.get(id=1)
+
+        self._trigger_validation_error_on_site_form(
+            s.name, s.domain, [templA.id], 'all_required', s.id)
+
+        templC.content = '{% invalid_tag %}'
+        templC.save()
+
+        self._trigger_validation_error_on_site_form(
+            s.name, s.domain, [templA.id], 'syntax_error', s.id)
+
+        templC.content = '{% extends "NonExistent" %}'
+        templC.save()
+
+        self._trigger_validation_error_on_site_form(
+            s.name, s.domain, [templA.id], 'required_not_exist', s.id)
+
+        templC.content = 'content'
+        templC.save()
+
+        p = Page(template="templD", site=s)
+        p.save()
+
+        self._trigger_validation_error_on_site_form(
+            s.name, s.domain, [templA.id, templB.id, templC.id],
+            'required_in_pages', s.id)
+
+        templD.sites.clear()
+
+        self._trigger_validation_error_on_site_form(
+            s.name, s.domain, [templA.id, templB.id, templC.id],
+            'required_in_pages', s.id)
+
+        Page.objects.filter(id=p.id).update(template='NonExistent')
+
+        self._trigger_validation_error_on_site_form(
+            s.name, s.domain, [templA.id, templB.id, templC.id],
+            'nonexistent_in_pages', s.id)
+
+        Page.objects.filter(id=p.id).update(template='templA')
+
+        templD.sites.add(s)
+
+        self._trigger_validation_error_on_site_form(
+            s.name, s.domain, [templA.id, templB.id, templC.id],
+            'orphan', s.id)
