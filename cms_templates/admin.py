fbfa602f88cb21520e6708d94d3f93105d39ee3c
Marcel Cojocaru <marcel.cojocaru@threepillarglobal.com>
2013-07-03 18:31:31 +0300

    LUN-625 -- transform the list into a set

--- cms_templates/admin.py
+++ cms_templates/admin.py
@@ -372,7 +372,7 @@ class ExtendedSiteAdminForm(SiteAdminForm):
 def _get_external_plugins_templates(site):
     templates = set([])
     for plugin_tuple in cms_tpl_settings.PLUGIN_TEMPLATE_REFERENCES:
-        templates |= _get_plugin_templates(site, plugin_tuple[0])
+        templates |= set(_get_plugin_templates(site, plugin_tuple[0]))
     return set(templates)
 
 
