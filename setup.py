1f450cbf5089b2038061e77ee3b4825e436caf73
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2013-09-12 16:00:14 +0300

    Bump version.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ dependency_links = [
 
 setup(
     name='django-cms-dbtemplates',
-    version='0.11',
+    version='0.12',
     description='Integrate django-cms and django-dbtemplates',
     long_description=open(README_PATH, 'r').read(),
     author='Sever Banesiu',
