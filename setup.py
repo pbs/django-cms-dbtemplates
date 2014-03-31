782190b9a616ba3c278951e348642e8bae481917
Andrei Horak <linkyndy@gmail.com>
2014-03-31 17:12:38 +0300

    Changed dependency_links egg versions
    So that dependency versions are fulfilled whilst specifying the dev version

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ dependencies = [
 
 
 dependency_links = [
-    'http://github.com/pbs/django-dbtemplates/tarball/develop#egg=django-dbtemplates-1.4.1pbs.dev',
-    'http://github.com/pbs/django-cms/tarball/support/2.3.x#egg=django-cms-2.3.5pbs.dev',
+    'http://github.com/pbs/django-dbtemplates/tarball/develop#egg=django-dbtemplates-1.4.1pbs.X.dev',
+    'http://github.com/pbs/django-cms/tarball/support/2.3.x#egg=django-cms-2.3.5pbs.X.dev',
     'http://github.com/pbs/django-admin-extend/tarball/master#egg=django-admin-extend-dev',
 ]
 
