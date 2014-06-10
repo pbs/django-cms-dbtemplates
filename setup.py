a27195380393fc54e313ac4a959b3e1f07f18d7f
Andrei Horak <linkyndy@gmail.com>
2014-06-10 15:33:54 +0300

    Bumped version from 0.16 to 0.17

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ dependency_links = [
 
 setup(
     name='django-cms-dbtemplates',
-    version='0.16',
+    version='0.17',
     description='Integrate django-cms and django-dbtemplates',
     long_description=open(README_PATH, 'r').read(),
     author='Sever Banesiu',
