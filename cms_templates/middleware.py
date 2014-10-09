dadc2acb6e4897a559ffb8fe8ad7325a16f7d8a0
pianas <paul.ianas@3pillarglobal.com>
2014-10-09 14:04:09 +0300

    LUN-1869: Since we are managing the state of the current site id, it is important to sync the CMS_TEMPLATES variables as sooner as possible in the request/response cycle, so that even on early middleware chain breaking, the error page will be displayed correctly with respect to the current site. This fix also ensures that we are able to show the CMS error page even if there is an early exception raised in the middleware chain

--- cms_templates/middleware.py
+++ cms_templates/middleware.py
@@ -38,6 +38,27 @@ def get_user_allowed_sites(user):
     return list(get_user_sites_queryset(user).values_list('id', flat=True))
 
 
+def _set_cms_templates_for_request(request):
+    '''Sets the CMS_TEMPLATES to the list of templates for the current site.
+
+    current site == session['cms_admin_site']
+    Thus this function makes sure CMS_TEMPLATES's value is correct
+    with respect to the current request.
+    '''
+    site_id = request.session.get('cms_admin_site', settings.SITE_ID)
+    try:
+        templates = get_site_templates(site_id)
+    except Site.DoesNotExist:
+        logger.error('Current site not found: %d. '
+                     'It was probably deleted' % site_id)
+        raise Http404
+    CMS_TEMPLATES = settings.__class__.CMS_TEMPLATES
+    CMS_TEMPLATES.value = [(templ.name, templ.name) for templ in templates]
+    if not CMS_TEMPLATES.value:
+        CMS_TEMPLATES.value = [('dummy',
+                                'Please create a template first.')]
+
+
 class SiteIDPatchMiddleware(object):
     """ This middleware works together with DynamicSiteIDMiddleware
     from djangotoolbox and patches the site id based on the
@@ -56,7 +77,18 @@ class SiteIDPatchMiddleware(object):
             logger.warning("SiteIDPatchMiddleware is raising {0}\n\n. "
                            "Using {1} and bubble up".format(e, self.fallback))
             self.fallback.process_request(request)
+            # any exception raised by the resolver must be raised further
             raise e
+        finally:
+            # try to set the correct value for CMS_TEMPLATES.
+            # this ensures we are able to correctly display the CMS error page
+            try:
+                _set_cms_templates_for_request(request)
+            except:
+                # do not interfere in the normal exception flow, since
+                # setting the correct CMS_TEMPLATES in this step is optional
+                pass
+
         user = getattr(request, 'user', None)
 
         if (match.app_name == 'admin'
@@ -114,18 +146,7 @@ def get_site_templates(site_id=None):
 
 class DBTemplatesMiddleware(object):
     def process_request(self, request):
-        site_id = request.session.get('cms_admin_site', settings.SITE_ID)
-        try:
-            templates = get_site_templates(site_id)
-        except Site.DoesNotExist:
-            logger.error('Current site not found: %d. '
-                         'It was probably deleted' % site_id)
-            raise Http404
-        CMS_TEMPLATES = settings.__class__.CMS_TEMPLATES
-        CMS_TEMPLATES.value = [(templ.name, templ.name) for templ in templates]
-        if not CMS_TEMPLATES.value:
-            CMS_TEMPLATES.value = [('dummy',
-                                    'Please create a template first.')]
+        _set_cms_templates_for_request(request)
 
         # This is a huge hack.
         # Expand the model choices field to contain all templates.
@@ -135,5 +156,6 @@ class DBTemplatesMiddleware(object):
             choices += [(settings.CMS_TEMPLATE_INHERITANCE_MAGIC,
                          CMS_TEMPLATE_INHERITANCE_TITLE)]
         Page._meta.get_field_by_name('template')[0].choices[:] = choices
+        CMS_TEMPLATES = settings.__class__.CMS_TEMPLATES
         CMS_TEMPLATES.value.append((settings.CMS_TEMPLATE_INHERITANCE_MAGIC,
                                     CMS_TEMPLATE_INHERITANCE_TITLE))
