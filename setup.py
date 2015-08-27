b676c3bcbf481a4ccb2f29c3428219fc0f1fe0af
bogdan-cornianu <bogdan.cornianu@3pillarglobal.com>
2015-08-27 17:46:43 +0300

    Bump version as instructed by Bamboo.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ dependency_links = [
 
 setup(
     name='django-cms-dbtemplates',
-    version='1.1.0',
+    version='1.2.0',
     description='Integrate django-cms and django-dbtemplates',
     long_description=open(README_PATH, 'r').read(),
     author='Sever Banesiu',
